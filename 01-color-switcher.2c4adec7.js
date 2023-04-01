53d9234e286c9be29dbd134aeb2457eb27a9fcbe
ZarytskyiSergii <114443256+ZarytskyiSergii@users.noreply.github.com>
2023-04-01 20:22:08 +0000

    Deploying to gh-pages from @ ZarytskyiSergii/parcel-project-goit-js-hw-09@019cb37e3e2de5c6e5b1792ee84ea0440dbea2a2 🚀

---
+++ 01-color-switcher.2c4adec7.js
@@ -0,0 +1,2 @@
+let t=null;const e=document.querySelector("[data-start]");e.addEventListener("click",(()=>{e.disabled=!0,t=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`;document.body.style.backgroundColor=t}),1e3)}));document.querySelector("[data-stop]").addEventListener("click",(()=>{clearInterval(t),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.2c4adec7.js.map
---
+++ 01-color-switcher.6e6f855a.js
@@ -0,0 +1,2 @@
+!function(){var t=null;var e=document.querySelector("[data-start]");e.addEventListener("click",(function(){e.disabled=!0,t=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"));document.body.style.backgroundColor=t}),1e3)})),document.querySelector("[data-stop]").addEventListener("click",(function(){clearInterval(t),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.6e6f855a.js.map
